6705baafe7ee16e1b7dff6d55324d440d8b67d16
Florencia Tabarez <fftabarez97@gmail.com>
2020-09-02 13:19:25 -0600

    Starting process to upload to herkou by setting up MLab connection in server.js.

--- googlebookssearch/server.js
+++ googlebookssearch/server.js
@@ -1,18 +1,27 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const routes = require("./routes/API.js");
 const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-// app.use(routes);
+// API Routes
+app.use(routes);
+// Any * requests go to index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// MLab
+// const MONOGDB_URI = "";
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
